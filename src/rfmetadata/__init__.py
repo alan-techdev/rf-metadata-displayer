c206b62349dac31d30ff6d8ee9d6c1bfce512bc3
iso <iso.techdev@gmail.com>
2026-09-14 11:43:55 +0300

    added parser in the main

--- src/rfmetadata/__init__.py
+++ src/rfmetadata/__init__.py
@@ -4,9 +4,9 @@
   
 """
 
-from rfmetadata.version import version as __version__
-
-__all__ = (
-    "__version__",
-    "main",
-)
+from .__version__ import __author__ as __author__
+from .__version__ import __description__ as __description__
+from .__version__ import __license__ as __license__
+from .__version__ import __title__ as __title__
+from .__version__ import __url__ as __url__
+from .__version__ import __version__ as __version__
---
+++ src/rfmetadata/__version__.py
@@ -0,0 +1,10 @@
+"""
+Application version
+Application Summary
+"""
+__title__ = "RFC Display"
+__description__ = "Radio Frequency Surveillance viewer"
+__url__ = "https://rf-metadata-displayer.readthedocs.io/en/latest/"
+__version__ = "1.0.1"
+__author__ = "Alan M - Iso H"
+__license__ = "AGPL-3.0-only"
