02df7fbaadbe3bc7d7e2b57631c49bbe46a0c794
iso <iso.techdev@gmail.com>
2026-09-22 11:48:24 +0300

    Cicd now installs missing library because of pyside6

--- tests/test_windows/test_query_result_window.py
+++ tests/test_windows/test_query_result_window.py
@@ -1,21 +1,20 @@
-#from PySide6.QtWidgets import QMainWindow
-#
-#from rfmetadata.windows.query_result_window import TableResultsWindow
-#
-#
-#def test_window_initialization(qtbot):
-#
-#    data =  [
-#                (90, 103.7, 20.54, '06-06-2025 12:16:18'),
-#                (91, 104.3, 20.18, '06-06-2025 12:16:18')
-#            ]
-#
-#    table = TableResultsWindow(data)
-#
-#    qtbot.addWidget(table)
-#
-#    assert isinstance(table, QMainWindow)
-#
-#    table.set_type(True)
-#    assert table.window_type
-#
+from PySide6.QtWidgets import QMainWindow
+
+from rfmetadata.windows.query_result_window import TableResultsWindow
+
+
+def test_window_initialization(qtbot):
+
+    data =  [
+                (90, 103.7, 20.54, '06-06-2025 12:16:18'),
+                (91, 104.3, 20.18, '06-06-2025 12:16:18')
+            ]
+
+    table = TableResultsWindow(data)
+
+    qtbot.addWidget(table)
+
+    assert isinstance(table, QMainWindow)
+
+    table.set_type(True)
+    assert table.window_type
